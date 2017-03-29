34c9c4f3714ee076be150ba0ffbaf4e549940255
liwei <fanxiaobin@liweideMacBook-Air.local>
2017-03-29 10:52:18 +0800

    Initial Commit

---
+++ RunTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunTime
+//
+//  Created by fanxiaobin on 2017/3/29.
+//  Copyright © 2017年 fanxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
